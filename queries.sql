0c4054a8d3b5afb69879acb48eb2f8848b4004a1
bitsysoft <77351531+bitsysoft@users.noreply.github.com>
2024-06-01 22:16:39 +0300

    Step 6, add files, update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,8 +1,8 @@
--- Данный запрос получает число всех покупателей в таблице customers (шаг 4)
+-- Число всех покупателей в таблице customers (шаг 4)
 -- В задании не сказано, чтобы были отобраны уникальные покупатели, поэтому distinct не использовался
 SELECT count(*) AS customers_count FROM customers;
 
--- Первый отчет: запрос формирует десятку лучших продавцов (шаг 5)
+-- Десятка лучших продавцов (шаг 5)
 -- Группировка по employee_id, а не по имени - страховка от однофамильцев
 -- INNER JOIN используется потому, что покупки непонятных продавцов мы учитывать не будем
 -- и покупки без продавцов, если такие есть, мы тоже учитывать не будем
@@ -17,8 +17,8 @@ GROUP BY e.employee_id
 ORDER BY income DESC
 LIMIT 10;
 
--- Второй отчет - информация о продавцах, чья средняя выручка за сделку (шаг 5)
--- меньше средней выручки за сделку по всем продавцам.
+-- Информация о продавцах, чья средняя выручка за сделку меньше
+-- средней выручки за сделку по всем продавцам (шаг 5).
 -- Сначала определяю среднюю выручку по всем продавцам,
 -- потом делаю CROSS JOIN и группирую по продавцу
 WITH total AS (
@@ -38,7 +38,7 @@ GROUP BY e.employee_id,	seller,	t.total_avg
 HAVING avg(s.quantity * p.price) < t.total_avg
 ORDER BY average_income;
 
--- Третий запрос: выручка продавцов по дням недели  (шаг 5)
+-- Выручка продавцов по дням недели (шаг 5)
 -- Группировка по employee_id, а не просто по имени - страховка от однофамильцев
 -- Группировка и по дню недели, и по номеру дня недели использовалась потому, что
 -- в результате нужно выдать день недели строкой, а в сортироввке использовать номер дня
@@ -61,3 +61,38 @@ FROM tbl
 GROUP BY tbl.seller, tbl.day_of_week, tbl.employee_id, tbl.day_num
 ORDER BY tbl.day_num, seller;
 
+-- Данные по количеству уникальных покупателей и выручке, которую они принесли (шаг 6)
+SELECT
+	to_char(s.sale_date, 'YYYY-MM') AS selling_month,
+	count(DISTINCT s.customer_id) AS total_customers,
+	floor(sum(s.quantity * p.price)) AS income
+FROM sales s
+INNER JOIN products p ON s.product_id = p.product_id 
+GROUP BY selling_month
+ORDER  BY selling_month;
+
+-- Покупатели, первая покупка которых была в ходе проведения акций (шаг 6)
+WITH t AS (
+	SELECT
+		s.customer_id,
+		s.sale_date,
+		s.sales_person_id,
+		p.price 
+	FROM sales s
+	INNER JOIN products p ON s.product_id = p.product_id
+),
+res AS (
+	SELECT DISTINCT
+		t.customer_id,
+		t.sale_date,
+		t.sales_person_id
+	FROM t
+	WHERE t.price = 0
+	AND NOT EXISTS (SELECT * FROM t AS t2 WHERE t2.customer_id = t.customer_id AND t2.sale_date < t.sale_date)
+)
+SELECT
+	(SELECT concat(first_name, ' ', last_name) FROM customers c WHERE c.customer_id = res.customer_id) AS customer,
+	res.sale_date,t
+	(SELECT concat(first_name, ' ', last_name) FROM employees e WHERE e.employee_id = res.sales_person_id) AS seller
+FROM res
+ORDER BY res.customer_id;
